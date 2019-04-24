3ec6de805a83b4611dc5701a3a89d235e67e2aaa
Brian Ketelsen <mail@bjk.fyi>
2019-04-24 17:08:17 -0400

    login working

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import App from './App.html'
 
-const app = new App({ target: document.body, props: { name: 'SvelteJS'} })
+
+const app = new App({
+    target: document.body,
+    props: {
+        name: 'SvelteJS'
+    }
+})
+
+// Instantiate the GoTrue auth client with an optional configuration
 
 export default app
